62a382d758ad3f2769ae5703472b15e9c246060b
Pankaj Kumar Sah <spankaj@athenahealth.com>
2022-04-03 16:44:53 +0530

    user table name changes

--- src/main/java/lms/repository/UserRepo.java
+++ src/main/java/lms/repository/UserRepo.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepo extends JpaRepository<User, String> {
-    @Query(value = "select * from user where reg_no = ?1", nativeQuery = true)
+    @Query(value = "select * from student where reg_no = ?1", nativeQuery = true)
     public User findByRegNo(String regNo);
 }
