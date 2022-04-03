41ef851eae5c21e1d37fb1be81a305533f3f77d1
Pankaj Kumar Sah <spankaj@athenahealth.com>
2022-04-03 15:54:11 +0530

    deployment changes

--- src/main/java/lms/repository/UserRepo.java
+++ src/main/java/lms/repository/UserRepo.java
@@ -4,7 +4,7 @@ import lms.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepo extends JpaRepository<User,Integer> {
+public interface UserRepo extends JpaRepository<User, String> {
     @Query(value = "select * from user where reg_no = ?1", nativeQuery = true)
     public User findByRegNo(String regNo);
 }
